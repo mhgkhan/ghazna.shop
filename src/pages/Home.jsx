87ab6436ac949018df4f4b2b2c4c10167a8a5a13
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2024-12-10 15:44:46 +0500

    dublicate

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,6 +14,10 @@ export default function Home() {
           <WelcomeMessage />
           <Hero />
           <CategoriesSection />
+          <Hero />
+          <CategoriesSection />
+          <Hero />
+          <CategoriesSection />
 
 
 
