f720965dc2f5f33613e5f201b7368f59cc31accb
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2024-12-16 21:26:52 +0500

    ADDED SOME STYLE TO MENU BUTTONS

--- src/components/ui/Menu.jsx
+++ src/components/ui/Menu.jsx
@@ -15,17 +15,17 @@ const Menu = () => {
                 <div className=" menu-area  p-1 flex items-center justify-center gap-5">
                     {
                         Array.from([
-                            { text: "Home", icon: <FaHome className="text-2xl text-black" />, link:"/" },
-                            { text: "Search", icon: <FaSearch className="text-2xl text-black" /> , link:"/search"},
-                            { text: "Wishlist", icon: <FaRegHeart className="text-2xl text-black" /> , link:"/wishlist"},
-                            { text: "Cart", icon: <FaCartArrowDown className="text-2xl text-black" />, link:"/cart" },
-                            { text: "Profile", icon: <FaRegUser className="text-2xl text-black" /> , link:"/profile"},
+                            { text: "Home", icon: <FaHome className="text-2xl " />, link:"/" },
+                            { text: "Search", icon: <FaSearch className="text-2xl " /> , link:"/search"},
+                            { text: "Wishlist", icon: <FaRegHeart className="text-2xl" /> , link:"/wishlist"},
+                            { text: "Cart", icon: <FaCartArrowDown className="text-2xl" />, link:"/cart" },
+                            { text: "Profile", icon: <FaRegUser className="text-2xl " /> , link:"/profile"},
 
                         ]).map((ele, ind) => {
                             return <Link key={ind} to={ele.link}>
-                                <div  className="menu-item p-1 cursor-pointer hover:shadow-md hover:shadow-gray-400 transition-all duration-300 flex flex-col items-center justify-center menu-item w-[45px] rounded-[20px 20px 5px 5px]" style={{ borderRadius: "20px 20px 5px 5px" }} >
+                                <div  className="menu-item py-1 px-2 text-black cursor-pointer hover:shadow-md hover:shadow-gray-400 transition-all duration-300 flex flex-col items-center justify-center menu-item w-[45px] hover:bg-orange-600 hover:text-white font-bold rounded-md" >
                                 {ele.icon}
-                                <span className='text-center text-black text-sm'>{ele.text}</span>
+                                <span className='text-center text-sm'>{ele.text}</span>
                             </div>
                             </Link>
                         })
