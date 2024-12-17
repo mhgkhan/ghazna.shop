98e36c9cfe1fb6739144035f232233823b9d0832
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2024-12-17 17:01:11 +0500

    added product page

---
+++ src/pages/Product.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Product = () => {
+  return (
+    <main className='px-2 md:px-0 min-h-screen'>
+        <div className="container mx-auto">
+
+            our product page 
+
+
+
+        </div>
+    </main>
+  )
+}
+
+export default Product
