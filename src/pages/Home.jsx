b4a93ad42b161ec96457f88c0def19dc8b3c358f
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2024-12-18 16:57:28 +0500

    Some Changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,36 +1,23 @@
 
-import { useContext, useEffect } from "react";
 import CategoriesSection from "../components/ui/pages/home/CategoriesSection";
 import Hero from "../components/ui/pages/home/Hero";
 import TrendingProductsSection from "../components/ui/pages/home/TrendingProductsSection";
 import WelcomeMessage from "../components/ui/pages/home/WelcomeMessage";
-import { useAppContext } from "../context/AuthContext";
 
 export default function Home() {
-
-  const useAuthCtx = useAppContext();
-
-  // console.log(useAuthCtx.token)
-  // useEffect(()=>{
-  //   useAuthCtx.storeToken("ghaznakhanisagoodboyinthisuniverse")
-  // }, [])
-
 
   return (
 
     <>
       <main className="bg-gray-100 min-h-screen">
-
-
         <div className="container mx-auto md:px-auto px-2">
+          
           <WelcomeMessage />
           <Hero />
           <CategoriesSection />
           <TrendingProductsSection />
 
-          <br /><br /><br /><br /><br />
         </div>
-
       </main>
 
 
