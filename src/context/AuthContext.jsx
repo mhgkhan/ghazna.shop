a022a1c298cc9d922a90f50320bd0df68459eb83
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2024-12-27 12:20:52 +0500

    Add token verification logic and redirect to email verification if unverified

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,4 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react";
+import { APIROUTES } from "../configs/ApiRoutes";
 
 
 export const AppContext = createContext()
@@ -7,21 +8,41 @@ export const AppContext = createContext()
 export const AppContextProvider = ({ children }) => {
 
 
-    const [authToken,setToken] = useState(localStorage.getItem("authToken"));
+    const [authToken, setToken] = useState(localStorage.getItem("authToken"));
 
     const storeToken = tok => setToken(localStorage.setItem("authToken", tok))
     const removeToken = () => setToken(localStorage.removeItem("authToken"))
 
+    // api to check token the user is verified or unverified return false or true 
+    const checkToken = async () => {
+        // call to api to check token 
+        try {
+            const request = fetch(`${APIROUTES.BASE_URL}api/auth/users/checktoken`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ token: authToken })
+            })
+
+            const response = await request.json();
+            if (response.success) return true
+            else return false
+        } catch (error) {
+            return false;
+        }
+    }
 
     useEffect(() => {
         setToken(localStorage.getItem("authToken"))
     }, [authToken, removeToken])
-    
+
 
     return <AppContext.Provider value={{
         token: authToken,
         storeToken,
-        removeToken
+        removeToken,
+        checkToken
     }}>
         {children}
     </AppContext.Provider>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useAppContext } from '../context/AuthContext'
+import { useNavigate } from 'react-router-dom';
 
 const Profile = () => {
+  const navigate = useNavigate();
 
-  
+  const [verify, setVerify] = useState(false);
+
+  const { checkToken } = useAppContext();
+
+  const check = async () => {
+    const res = await checkToken();
+    if (res) setVerify(true)
+    else {
+      setVerify(false)
+      navigate("/checkemailverification");
+    }
+  }
+
+  useEffect(() => { check(), [] })
 
 
   return (
     <div>
-      this is profile 
+      this is profile
     </div>
   )
 }
