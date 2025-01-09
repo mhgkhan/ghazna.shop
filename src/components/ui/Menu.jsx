b70e9af695ddd8bf769adad016cd587959b1a07d
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2025-01-09 08:14:37 +0500

    added banner click to change event

--- src/components/ui/Menu.jsx
+++ src/components/ui/Menu.jsx
@@ -8,27 +8,27 @@ const Menu = () => {
 
     // get current pathname 
     const location = useLocation()
-        
+
 
 
     return (
         <div className=' z-50 text-xl bg-white border border-t border-t-2 border-t-gray-200 sticky bottom-0'>
             <div className="container mx-auto">
-                <div className=" menu-area  p-1 flex items-center justify-center gap-5">
+                <div className=" menu-area  p-1 flex items-center justify-center md:gap-5 gap-2">
                     {
                         Array.from([
-                            { text: "Home", icon: <FaHome className="text-2xl " />, link:"/" },
-                            { text: "Search", icon: <FaSearch className="text-2xl " /> , link:"/search"},
-                            { text: "Wishlist", icon: <FaRegHeart className="text-2xl" /> , link:"/wishlist"},
-                            { text: "Cart", icon: <FaCartArrowDown className="text-2xl" />, link:"/cart" },
-                            { text: "Profile", icon: <FaRegUser className="text-2xl " /> , link:"/profile"},
+                            { text: "Home", icon: <FaHome className="text-2xl " />, link: "/" },
+                            { text: "Search", icon: <FaSearch className="text-2xl " />, link: "/search" },
+                            { text: "Wishlist", icon: <FaRegHeart className="text-2xl" />, link: "/wishlist" },
+                            { text: "Cart", icon: <FaCartArrowDown className="text-2xl" />, link: "/cart" },
+                            { text: "Profile", icon: <FaRegUser className="text-2xl " />, link: "/profile" },
 
                         ]).map((ele, ind) => {
                             return <Link key={ind} to={ele.link}>
-                                <div  className={`menu-item py-1 px-2 text-black cursor-pointer hover:shadow-md hover:shadow-gray-400 transition-all duration-300 flex flex-col items-center justify-center menu-item w-[45px] hover:bg-orange-600 hover:text-white ${location.pathname == ele.link ? "bg-orange-600 text-white":""} font-bold rounded-md`} >
-                                {ele.icon}
-                                <span className='text-center text-sm'>{ele.text}</span>
-                            </div>
+                                <div className={`menu-item py-1 px-2 text-black cursor-pointer hover:shadow-md hover:shadow-gray-400 transition-all duration-300 flex flex-col items-center justify-center menu-item w-[50px] hover:bg-orange-600 hover:text-white ${location.pathname == ele.link ? "bg-orange-600 text-white" : ""} font-bold rounded-md`}  >
+                                    {ele.icon}
+                                    <span className='text-center text-sm'>{ele.text}</span>
+                                </div>
                             </Link>
                         })
                     }
--- src/components/ui/pages/home/Hero.jsx
+++ src/components/ui/pages/home/Hero.jsx
@@ -1,17 +1,60 @@
+import { useEffect, useRef, useState } from "react"
 
 const Hero = () => {
-    return (
-        <section className="hero rounded-3xl my-3 relative w-[99%] mx-auto md:w-full h-auto bg-orange-800 flex items-center justify-between p-5">
+    const ref = useRef()
 
-            <div className="text flex items-center justify-center flex-col  w-auto md:w-[50%]">
-                <h2 className={` text-xl md:text-4xl text-white text-center`}> 25% off with code </h2>
-                <button className="bg-white text-black mx-auto mt-2 font-bold md:text-lg rounded-full px-2 md:py-1">Shop now </button>
+
+    const [items, setItems] = useState([
+        { title: "25% off with code", link: "this is the link", img: "https://th.bing.com/th/id/R.66459278341bddfed30c782bd30a8aa7?rik=uIdUpHdp4tDETg&riu=http%3a%2f%2fclipart-library.com%2fimages_k%2fpurse-transparent-background%2fpurse-transparent-background-9.png&ehk=aa3IPZyw3wsPQpEs70tEI%2bBM9fGCXdFZAfWQg79Kp4Q%3d&risl=&pid=ImgRaw&r=0" },
+        { title: "Purchase for 250$ off 10%", link: "this is the link", img: "https://th.bing.com/th/id/R.66459278341bddfed30c782bd30a8aa7?rik=uIdUpHdp4tDETg&riu=http%3a%2f%2fclipart-library.com%2fimages_k%2fpurse-transparent-background%2fpurse-transparent-background-9.png&ehk=aa3IPZyw3wsPQpEs70tEI%2bBM9fGCXdFZAfWQg79Kp4Q%3d&risl=&pid=ImgRaw&r=0" },
+        { title: "15% off with code ", link: "this is the link", img: "https://th.bing.com/th/id/R.66459278341bddfed30c782bd30a8aa7?rik=uIdUpHdp4tDETg&riu=http%3a%2f%2fclipart-library.com%2fimages_k%2fpurse-transparent-background%2fpurse-transparent-background-9.png&ehk=aa3IPZyw3wsPQpEs70tEI%2bBM9fGCXdFZAfWQg79Kp4Q%3d&risl=&pid=ImgRaw&r=0" }
+    ])
+
+    const [currentHero, setCurrentHero] = useState(items[0])
+    const [ani, setAni] = useState(false)
+
+    const changeHero = ind => {
+        setAni(true)
+        ref.current.classList.remove("hero-banner")
+        setCurrentHero(items[ind])
+        setTimeout(() => setAni(false), 500)
+        // ref.current.classList.add("hero-banner")
+    }
+
+    // const [counter, setCounter] = useState(0)
+
+    // setInterval(() => {
+    //     if (counter <=2) {
+    //         changeHero(counter)
+    //         setCounter(counter + 1)
+    //     }
+    //     else {
+    //         setCounter(0)
+    //     }
+    // }, 2000);
+
+    useEffect(() => {
+        changeHero(0)
+    }, [])
+
+    return (
+        <section className="hero">
+
+            <div ref={ref} className={`transition-all duration-500 ${ani ? "opacity-0" : "opacity-100"} ero-banner flex items-center justify-between rounded-3xl my-3 relative w-[99%] mx-auto md:w-full h-auto bg-orange-800 p-5`}>
+                <div className="text flex items-center justify-center flex-col  w-auto md:w-[50%]">
+                    <h2 className={` text-xl md:text-4xl text-white text-center`}> {currentHero && currentHero.title} </h2>
+                    <button className="bg-white text-black mx-auto mt-2 font-bold md:text-lg rounded-full px-2 md:py-1">Shop now </button>
+                </div>
+
+                <div className="image w-[35%] md:w-[50%] md:px-10 relative md:scale-[1] ">
+                    <img width={250} height={150} alt="product picture" src={currentHero && currentHero.img}
+                        className="float-right md:scale-[1]"
+                    />
+                </div>
             </div>
 
-            <div className="image w-[35%] md:w-[50%] md:px-10 relative md:scale-[1] ">
-                <img width={250} height={150} alt="product picture" src={"https://th.bing.com/th/id/R.66459278341bddfed30c782bd30a8aa7?rik=uIdUpHdp4tDETg&riu=http%3a%2f%2fclipart-library.com%2fimages_k%2fpurse-transparent-background%2fpurse-transparent-background-9.png&ehk=aa3IPZyw3wsPQpEs70tEI%2bBM9fGCXdFZAfWQg79Kp4Q%3d&risl=&pid=ImgRaw&r=0"}
-                    className="float-right md:scale-[1]"
-                />
+            <div className="dots flex items-center justify-center gap-2">
+                {items.map((ele, ind) => <div onClick={() => changeHero(ind)} key={ele + Math.floor(Math.random() * 5000)} className="p-2 bg-gray-500 rounded-full cursor-pointer hover:bg-gray-800"></div>)}
             </div>
 
         </section>
